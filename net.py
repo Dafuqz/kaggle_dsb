30bb30a4b76e90946a54851c8bbad030f3bed548
jackyzha0 <jacky.zhao2019@stgeorges.bc.ca>
2018-01-27 12:35:25 -0800

    2nd

--- net.py
+++ net.py
@@ -1,2 +1,17 @@
 # !/usr/local/bin/python
 import sugartensor as tf
+import numpy as np
+import util
+
+# PARAMS #
+num_layers = 3
+activation = tf.nn.relu
+batchsize = 8
+path_to_train_data = "__data/stage1_train"
+path_to_test_data = "__data/stage1_test"
+
+img_dat = util.crawl_path(path_to_train_data)
+
+
+def cnn(features,labels):
+ input_layer = tf.reshape(features, [-1, 28, 3])
--- util.py
+++ util.py
@@ -1,28 +1,26 @@
 # !/usr/local/bin/python
-def sparse_tuple_from(sequences, dtype=np.int32):
-    """
-    Author: github.com/igormq
-    Create a sparse representention of input array. For handling one-hot vector in targets
-    Args:
-        sequences: a list of lists of type dtype where each element is a sequence
-    Returns:
-        A tuple with (indices, values, shape)"""
-    indices = []
-    values = []
-    for i, seq in enumerate(sequences):
-        indices.extend(zip([i]*len(seq), range(len(seq))))
-        values.extend(seq)
+import glob
+from PIL import Image
 
-    indices = np.asarray(indices, dtype=np.int64)
-    values = np.asarray(values, dtype=dtype)
-    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1]+1], dtype=np.int64)
-
-    return indices, values, shape
-
+def padimg(arr, max_h=2048, max_w=2048):
+    print('call to',self)
+def preprocess():
+    #if img avg colour not approx grayscale
+    #invert
+    #add rotation / noise / contrast
+    print('call to',self)
 def next_miniBatch():
-    
-def next_set():
-
+    print('call to',self)
+def next_set(height,width):
+    print('call to',self)
 def get_label():
-
+    print('call to',self)
 def get_img():
+    print('call to',self)
+def crawl_path(path):
+    db = []
+    for __file in glob.iglob(path + '/*/*/*.*'):
+        im = Image.open(__file)
+        print([__file,im.size[0],im.size[1]])
+        db.append([__file,im.size[0],im.size[1]])
+    return db
