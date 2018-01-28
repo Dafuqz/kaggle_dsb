4f1d80ed49efda9f594e53db7f03e20d10d800d8
FizzesShark <xbug2001@gmail.com>
2018-01-27 22:04:20 -0800

    Added work-in-progress image processing function

--- util.py
+++ util.py
@@ -4,7 +4,25 @@ from PIL import Image
 
 def padimg(arr, max_h=2048, max_w=2048):
     print('call to',self)
-def preprocess():
+def preprocess(path):
+    img = Image.open(path)
+    img = img.convert('RGB')
+    finalArr = []
+    overall = 0
+    w, h = img.size
+    for i in range(0, w):
+        finalArr.append([])
+        for j in range(0, h):
+            pixelr, pixelg, pixelb = img.getpixel((i, j))
+            #print(pixelr, pixelg, pixelb)
+            finalArr[i].append((pixelr + pixelg + pixelb) / 3)
+            overall += (pixelr + pixelg + pixelb) / 3
+    overall /= w * h
+    if (overall > 25):
+        for i in range(0, w):
+            for j in range(0, h):
+                finalArr[i][j] = 255 - finalArr[i][j]
+    return finalArr
     #if img avg colour not approx grayscale
     #invert
     #add rotation / noise / contrast
@@ -24,3 +42,5 @@ def crawl_path(path):
         print([__file,im.size[0],im.size[1]])
         db.append([__file,im.size[0],im.size[1]])
     return db
+
+print(preprocess("00ae65c1c6631ae6f2be1a449902976e6eb8483bf6b0740d00530220832c6d3e.png"))
